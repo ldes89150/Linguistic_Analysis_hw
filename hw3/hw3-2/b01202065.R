b7650744c7ee2b21c4c6e0303642fca93e962aeb
cys <ldes89150@gmail.com>
2015-10-12 12:25:12 +0800

    fix hw3-2

--- hw3/hw3-2/b01202065.R
+++ hw3/hw3-2/b01202065.R
@@ -1,6 +1,31 @@
 system("wget -N https://ceiba.ntu.edu.tw/course/6d0f76/content/1001_chinatimes.txt")
 system("wget -N https://ceiba.ntu.edu.tw/course/6d0f76/content/jieba.R")
-system("cat 1001_chinatimes.txt | Rscript jieba.R | tr '\n' ' ' > b01202065.seg")
-system("cat b01202065.seg | tr ' ' '\n' | sort | uniq -c | sort -r > b01202065.csv")
+
+text=scan(file='1001_chinatimes.txt',what='char')
+library(jiebaR)
+words_vector=worker()<=text
+words_vector
+words_char=paste(words_vector,collapse=' ')
+write(x=words_char,file='b01202065.seg')
+
+library(tm)
+
+vs <- VectorSource(words_vector)
+vs
+
+txt <- Corpus(vs)
+tdm<- TermDocumentMatrix(txt,control = list(wordLengths = c(1, Inf) ))
+temp <- inspect(tdm)
+FreqMat <- data.frame(row.names = rownames(temp), Freq = rowSums(temp))
+
+FreqMat = FreqMat[order(FreqMat$Freq,decreasing = TRUE),,drop=FALSE]
+FreqMat
+
+as.matrix(FreqMat)
+
+write.table(FreqMat, file='b01202065.csv',col.names = FALSE, quote = FALSE)
+
 system("rm 1001_chinatimes.txt")
 system("rm jieba.R")
+
+
